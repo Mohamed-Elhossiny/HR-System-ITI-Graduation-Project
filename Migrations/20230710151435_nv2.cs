137d19581768b91ea536ae2cfc15eb07b1038374
Mohamed Elhossiny <Mohamed.saed.elhossiny@gmail.com>
2023-07-12 16:00:12 +0300

    Add project files.

---
+++ Migrations/20230710151435_nv2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HrProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class nv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Departments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Departments");
+        }
+    }
+}
